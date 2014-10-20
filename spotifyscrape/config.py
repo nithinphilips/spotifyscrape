869850577116d095751fa40feb2da2b9498c4cdf
Nithin Philips <nithin@nithinphilips.com>
2014-10-20 14:02:27 -0400

    Removing unnecessary check for config file.

--- spotifyscrape/config.py
+++ spotifyscrape/config.py
@@ -7,11 +7,8 @@ config = None
 def read_config():
     global config
     if not config:
-        if os.path.exists(APP_CONFIG_FILE):
-            config = ConfigParser.SafeConfigParser()
-            config.readfp(open(os.path.join(os.path.dirname(__file__), 'defaults.conf')))
-            config.read(['/etc/spotifyscrape.conf', APP_CONFIG_FILE])
-        else:
-            logging.debug("Config file '{}' not found".format(APP_CONFIG_FILE))
+        config = ConfigParser.SafeConfigParser()
+        config.readfp(open(os.path.join(os.path.dirname(__file__), 'defaults.conf')))
+        config.read(['/etc/spotifyscrape.conf', APP_CONFIG_FILE])
 
     return config
